c042ea4e459b364770a81cfc44b2d60b5317ae76
NicolaM94 <nicola.moro2312@gmail.com>
2024-02-29 19:22:21 +0100

    starting login handler

--- handlers/loginhandler.go
+++ handlers/loginhandler.go
@@ -1,7 +1,16 @@
 package handlers
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
+// Catches and responds to login.html
 func LoginReqHanlder(w http.ResponseWriter, r *http.Request) {
 
+	// Retrieves cookies for authentication
+	ck := r.Cookies()
+	for c := range ck {
+		fmt.Println(ck[c].Value)
+	}
 }
